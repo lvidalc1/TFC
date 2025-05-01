ebc93818e356a5591a5b8c24c19070d6d8eb5471
lvidalc1 <lvidalc22@fpcoruna.afundacion.org>
2025-05-02 01:21:04 +0200

    Creación componente categorías y login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CategoriasComponent } from './categorias/categorias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CategoriasComponent
   ],
   imports: [
     BrowserModule,
