e6ac33c088da081dd445e0eb6a6ab65fa19eef88
lvidalc1 <lvidalc22@fpcoruna.afundacion.org>
2025-05-02 01:51:02 +0200

    Adición rutas de login y categorías

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriasComponent } from './categorias/categorias.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'categorias', component: CategoriasComponent },
+  { path: 'login', component: LoginComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
