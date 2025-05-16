b9392a9b6100072beb003ce04816f3b82cc8f1cf
lvidalc1 <lvidalc22@fpcoruna.afundacion.org>
2025-05-16 22:01:53 +0200

    Diseño responsive botones cabecera

--- src/app/shared/menu-lateral/menu-lateral.component.ts
+++ src/app/shared/menu-lateral/menu-lateral.component.ts
@@ -13,9 +13,10 @@ export class MenuLateralComponent implements OnInit {
   }
 
   selectedMenu: string = '';
+  //selectedMenu es una propiedad que guarda el nombre de la opción del menú seleccionada, para así poder usar la clase activo.
 
   selectMenu(menu: string) {
     this.selectedMenu = menu;
-  }
+  }//este método cambia el valor de selectedMenu por el nombre de la opción de cada div, que se pasa como parámetro.
 
 }
